df2ee2d346a36c296a5a1eb2c52487dedf491096
iivdev <climov.van@yandex.ru>
2022-02-13 19:02:10 +0700

    Changed endpoints

--- main.go
+++ main.go
@@ -5,16 +5,17 @@ import (
 	"log"
 	"net/http"
 )
+
 func main() {
 	err := ReadConfig()
-	if err!=nil{
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	initDBs()
 	http.HandleFunc("/auth/register", registerHandler)
 	http.HandleFunc("/auth/confirm", confirmHandler)
-	http.HandleFunc("/auth/ping", pingHandler)
+	http.HandleFunc("/ping", pingHandler)
 	http.HandleFunc("/auth/checkAuth", checkAuthHandler)
-	log.Fatal(http.ListenAndServe(":8080",  nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
